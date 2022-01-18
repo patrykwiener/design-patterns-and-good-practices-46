546fe7ee3046a07450fa3ec06f60056a69dfd9ac
pwiener <patryk.wiener@siroccomobile.com>
2022-01-18 19:01:50 +0100

    pylint

--- pep8/pylint_exercise.py
+++ pep8/pylint_exercise.py
@@ -1,45 +1,37 @@
 # pylint: disable=missing-module-docstring missing-class-docstring missing-function-docstring
-import timeit
 
-class SAMPLE_PYLINT:  # pylint: disable=too-few-public-methods
+
+class SamplePylint:  # pylint: disable=too-few-public-methods
     def __init__(self, number):
-        self._NUMBER = number
+        self._number = number
 
-    def Divide(cls, number):
+    @classmethod
+    def divide(cls, number):
         if number == 0:
             raise ZeroDivisionError()
 
 
-class children(SAMPLE_PYLINT):
+class Children(SamplePylint):
 
-    def __init__(self, name, PARAM, *args, **kwargs):
+    def __init__(self, name, number):
+        super().__init__(number)
         self._name = name
 
-    def some_method(self, param):
-        if param == 1:
-            return  True
-        elif param == 2:
-            return True
-        elif param        ==3:
-            return True
-        elif param ==3:
-            return True
-        elif param ==3:
-            return True
-        else:
-            return False
+    @classmethod
+    def some_method(cls, param):
+        return param in (1, 2, 3)
 
-    def some_method2():
+    @classmethod
+    def some_method2(cls):
         print('bad implementation')
 
 
 if __name__ == '__main__':
-    sample = SAMPLE_PYLINT(10)
+    sample = SamplePylint(10)
     try:
-        sample.Divide(0)
-    except Exception:
+        sample.divide(0)
+    except ZeroDivisionError:
         pass
-    sample.Divide()
+    sample.divide(number=10)
 
-    obj = children
-    obj.some_method(4)
+    Children.some_method(4)
