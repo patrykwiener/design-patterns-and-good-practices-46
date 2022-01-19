d4a6bac1889a0719ee6368cf345d4e36ebaaef4c
pwiener <patryk.wiener@siroccomobile.com>
2022-01-19 21:06:01 +0100

    liskov substitution principle

--- solid/liskov_substitution_principle.py
+++ solid/liskov_substitution_principle.py
@@ -7,18 +7,29 @@ class DeliveryBase(ABC):
         self.product_ids = product_ids
         self.user = user
 
+
+class OfflineDelivery(DeliveryBase, ABC):
     @abstractmethod
     def get_delivery_location(self):
         pass
 
 
-class ComputerDelivery(DeliveryBase):
+class OnlineDelivery(DeliveryBase, ABC):
+    @abstractmethod
+    def get_delivery_email(self):
+        pass
+
+
+class ComputerDelivery(OfflineDelivery):
 
     def get_delivery_location(self):
         pass
 
 
-class SoftwareDelivery(DeliveryBase):
+class SoftwareDelivery(OnlineDelivery):
 
-    def get_delivery_location(self):  # cannot be implemented! It's senseless!
+    def get_delivery_email(self):
         pass
+
+
+software_delivery = SoftwareDelivery(product_ids=[1, 2, 3], user='jakis user')
