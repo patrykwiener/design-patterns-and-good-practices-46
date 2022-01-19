2b6bf762cda2835e12b351f8e5587550ffad4776
pwiener <patryk.wiener@siroccomobile.com>
2022-01-19 20:15:34 +0100

    open-closed principle

--- solid/open_closed_principle.py
+++ solid/open_closed_principle.py
@@ -1,21 +1,46 @@
-class EasterDiscount:
+from abc import ABC, abstractmethod
+
+
+class Discount(ABC):
+
+    @abstractmethod
+    @property
+    def discount(self):
+        pass
+
+
+class EasterDiscount(Discount):
     _EASTER_DISCOUNT = '30%'
 
-    def get_easter_discount(self):
+    @property
+    def discount(self):
         return f'{self._EASTER_DISCOUNT} on Easter'
 
 
-class BlackFridayDiscount:
+class BlackFridayDiscount(Discount):
     _BLACK_FRIDAY_DISCOUNT = '50%'
 
-    def get_black_friday_discount(self):
+    @property
+    def discount(self):
         return f'{self._BLACK_FRIDAY_DISCOUNT} on Black Friday'
+
+
+class MothersDayDiscount(Discount):
+    _MOTHERS_DAY_DISCOUNT = '15%'
+
+    @property
+    def discount(self):
+        return f'{self._MOTHERS_DAY_DISCOUNT} on Mother\'s day'
 
 
 class DiscountManager:
 
-    def process_easter_discount(self, easter_discount: EasterDiscount):
+    def process_discount(self, discount: Discount):
         pass
 
-    def process_black_friday_discount(self, black_friday_discount: BlackFridayDiscount):
-        pass
+
+mothers_discount = MothersDayDiscount()
+black_discount = BlackFridayDiscount()
+discount_manager = DiscountManager()
+discount_manager.process_discount(discount=mothers_discount)
+discount_manager.process_discount(discount=black_discount)
